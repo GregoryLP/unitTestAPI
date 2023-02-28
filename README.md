4b20d2a8e8bfd8bda56be3012d40b997cdb47d28
grégory LOMBARD--PARADIS <gregory332000@hotmail.fr>
2023-02-28 17:22:16 +0100

    Ajout ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # unitTestAPI
+
+## Pré requis et installation
+
+Cette APi fonctionne avec MONGODB, une fois fastAPI lancer celle-ci va se créer toute seul.
+
+Pour l'installation vous aurez besoin de différent ne commençant par "pip install", faites ceux-ci pour chacun des packages suivants : fastapi, uvicorn, unitest, requests
+
+exemple : 
+
+`````
+pip install fastapi
+`````
+
+## Utilisation
+
+Pour utiliser cette API vous devez la lancer avec cette commande :
+
+``````
+python app/main.py
+``````
+Puis avec votre navigateur préféré aller sur l'url ci-dessous
+``````
+http://127.0.0.1:8000/
+``````
+
+## Test
+
+Pour lancer veuillez excuter cette commande :
+````
+cd test/unit/app
+python -m unittest -v apiTest
+````
--- test/unit/app/apiTest.py
+++ test/unit/app/apiTest.py
@@ -4,7 +4,7 @@ import requests
     # URL de l'API
 url = 'http://127.0.0.1:8000/classeDestiny'
 
-data = {
+data_classeDestiny = {
     "name": "Titan Test",
     "doctrine": "Solaire test",
     "element": "Feu test"
@@ -13,38 +13,38 @@ data = {
 class TestAPI(unittest.TestCase):
 
     def setUp(self):
-        self.classeDestiny_data = data
+        self.classeDestiny_data = data_classeDestiny
 
     def test_ajout(self):
-        response = requests.post(url, data=data)
+        response = requests.post(url, json=data_classeDestiny)
         self.assertEqual(response.status_code, 200)
         classeDestiny = response.json()
         self.assertIsInstance(classeDestiny, dict)
-        self.assertEqual(classeDestiny['data'][0]["name"], self.classeDestiny_data["name"])
-        self.assertEqual(classeDestiny['data'][0]["doctrine"], self.classeDestiny_data["name"])
-        self.assertEqual(classeDestiny['data'][0]["element"], self.classeDestiny_data["name"])
+        self.assertEqual(classeDestiny['data_classeDestiny'][0]["name"], self.classeDestiny_data["name"])
+        self.assertEqual(classeDestiny['data_classeDestiny'][0]["doctrine"], self.classeDestiny_data["name"])
+        self.assertEqual(classeDestiny['data_classeDestiny'][0]["element"], self.classeDestiny_data["name"])
     
     def test_update(self):
         self.classeDestiny_data['name'] = 'Chasseur_modifier'
         response = requests.get(url)
         old_classeDestiny = response.json()
         for i in range(0, len(old_classeDestiny['data'][0])):
-            if old_classeDestiny['data'][i]['name'] == 'Titan Test':
-                id_classeDestiny = old_classeDestiny['data'][i]['id']
+            if old_classeDestiny['data_classeDestiny'][i]['name'] == 'Titan Test':
+                id_classeDestiny = old_classeDestiny['data_classeDestiny'][i]['id']
             break
-        response = requests.put(f"{url}/{id_classeDestiny}", json=data)
+        response = requests.put(f"{url}/{id_classeDestiny}", json=data_classeDestiny)
         self.assertEqual(response.status_code, 200)
         classeDestiny = response.json()
         self.assertIsInstance(classeDestiny, dict)
-        self.assertEqual(classeDestiny['data'][0], f"Classe Destiny {id_classeDestiny} modifié avec succès")
+        self.assertEqual(classeDestiny['data_classeDestiny'][0], f"Classe Destiny {id_classeDestiny} modifié avec succès")
     
     def test_suppression(self):
         
         response = requests.get(url)
         old_classeDestiny = response.json()
-        for i in range(0, len(old_classeDestiny['data'][0])):
-            if old_classeDestiny['data'][i]['name'] == 'Titan Test':
-                id_classeDestiny = old_classeDestiny['data'][i]['id']
+        for i in range(0, len(old_classeDestiny['data_classeDestiny'][0])):
+            if old_classeDestiny['data_classeDestiny'][i]['name'] == 'Titan Test':
+                id_classeDestiny = old_classeDestiny['data_classeDestiny'][i]['id']
             break
         response = requests.delete(f"{url}/{id_classeDestiny}")
         self.assertEqual(response.status_code, 200)
